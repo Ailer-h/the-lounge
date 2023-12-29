c7d34b594447ce7d16148e7f1819463d7aa01cd0
Ailer-h <henriqueailer2@gmail.com>
2023-12-29 13:44:26 -0300

    ok

---
+++ the lounge project.bat
@@ -0,0 +1,10 @@
+git config --global user.email "henriqueailer2@gmail.com"
+git config --global user.name "Ailer-h"
+git clone https://github.com/Ailer-h/the-lounge.git
+cd the-lounge
+
+git add --all
+git commit -m "ok"
+git pull
+git fetch
+git push
